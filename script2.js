a960faded07b839afa9c201996892c97f1b50189
IrynaJuriivna <45572834+IrynaJuriivna@users.noreply.github.com>
2020-11-06 13:18:04 +0200

    fo

--- script2.js
+++ script2.js
@@ -2,7 +2,12 @@ var rez = document.getElementById('var_x');
 function calc_click() {
     var a = document.getElementById('var_a').value;
     var b = document.getElementById('var_b').value;
-    var c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+    if(a==0 || b==0){
+        var c="Катетет не може бути рівним 0! Введіть інше значення.";
+    }
+    else{
+        var c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+    }
     rez.innerHTML = c;
 }
 
