40c4f861a451a351c23e0bb88cee87075402bafe
IrynaJuriivna <45572834+IrynaJuriivna@users.noreply.github.com>
2020-11-06 13:18:55 +0200

    f

--- script2.js
+++ script2.js
@@ -3,7 +3,7 @@ function calc_click() {
     var a = document.getElementById('var_a').value;
     var b = document.getElementById('var_b').value;
     if(a==0 || b==0){
-        var c="Катетет не може бути рівним 0! Введіть інше значення.";
+        var c="  Катетет не може бути рівним 0! Введіть інше значення.";
     }
     else{
         var c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
