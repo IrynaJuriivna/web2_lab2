28c18fcdebfe85881cb01ea43ca944ff8c0a16d4
IrynaJuriivna <45572834+IrynaJuriivna@users.noreply.github.com>
2020-11-06 13:30:46 +0200

    ssss

--- script2.js
+++ script2.js
@@ -2,7 +2,7 @@ var rez = document.getElementById('var_x');
 function calc_click() {
     var a = document.getElementById('var_a').value;
     var b = document.getElementById('var_b').value;
-    if(a==0 || b==0){
+    if(a==0 || b==0 || a<0 || b<0){
         var c="  Катетет не може бути рівним 0! Введіть інше значення.";
     }
     else{
