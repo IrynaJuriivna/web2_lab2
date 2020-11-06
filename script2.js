294525c4cca1cd06f1bbe1f5a49bec01c4765ced
IrynaJuriivna <45572834+IrynaJuriivna@users.noreply.github.com>
2020-11-06 12:55:39 +0200

    sec

--- script2.js
+++ script2.js
@@ -1,19 +1,9 @@
 var rez = document.getElementById('var_x');
-console.log("start");
-
 function calc_click() {
     var a = document.getElementById('var_a').value;
     var b = document.getElementById('var_b').value;
-
-        if (a > b) {
-            var c = Math.sqrt(Math.pow(a, 2) - Math.pow(b, 2));
-        }
-        else {
-            var c = "Гіпотенуза не може бути меншою від катета!"
-        }
-
-        rez.innerHTML = c;
-    
+    var c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+    rez.innerHTML = c;
 }
 
 
